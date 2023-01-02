dd545204d6bd8abafea9a82ce3bbacb9b9a52718
Kitesa <kitesamerga@gmail.com>
2023-01-02 06:46:31 -0800

    Fixed betty

--- src/drawEvent.c
+++ src/drawEvent.c
@@ -1,156 +1,156 @@
 #include "../headers/header.h"
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
-double playerA = 0.0;
-/* screen dimension contants */
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
 void *pixels = NULL;
-int y;
-int x;
-/* 
- * mouseEvents - 
- * @mouse_event: mouse event
- * Description:
- * Return: false
- */
-bool mouseEvents(SDL_Event mouse_event){
-	/* Assume speed at which the player can rotate */
-	const double player_angular_speed_of_rotation = 0.025;
-		
-	/* mouse events for 360 degree rotation */
-	while(SDL_PollEvent(&mouse_event) != 0){
-		if (mouse_event.type == SDL_QUIT){
-			return true;
-		}
-		if (mouse_event.type == SDL_MOUSEMOTION){
-			/* relative motion in the X direction times angular speed*/
-			playerA += mouse_event.motion.xrel * player_angular_speed_of_rotation;
-		}
-	}
-	return false;
-}
-
-/*
- * handleAllEvents - 
+double plA = 0.0;
+/**
+ * handleAllEvents - function to handle keyboard and mouse event
  * @e: event
- * Description:
+ * Description: handles all events
  * Return: false
  */
- 
-bool handleAllEvents(SDL_Event e){
-	const double player_linear_speed= 0.0875;
-	const Uint8* currentKeyStates;
 
+bool handleAllEvents(SDL_Event e)
+{
+	const double sp = 0.0875, player_ang_speed = 0.025;
+	const Uint8 *currentKeyStates;
+
+	while (SDL_PollEvent(&e) != 0)
+	{
+		if (e.type == SDL_QUIT)
+			return (true);
+		if (e.type == SDL_MOUSEMOTION)
+			plA += e.motion.xrel * player_ang_speed;
+	}
 	currentKeyStates = SDL_GetKeyboardState(0);
-	/* call mouse based event for 360 degree rotation of a player */
-	mouseEvents(e);
-	/* scan keyboard events */
-	if (currentKeyStates[SDL_SCANCODE_ESCAPE]){
-		return true;
+	if (currentKeyStates[SDL_SCANCODE_ESCAPE])
+	{
+		return (true);
 	}
-	if(currentKeyStates[SDL_SCANCODE_UP]){
-		double delta_x = cos(playerA) * player_linear_speed;
-		double delta_y = sin(playerA) * player_linear_speed;
-		playerMovementControl(delta_x, delta_y);
-	}
-	if(currentKeyStates[SDL_SCANCODE_DOWN]){
-		double delta_x = cos(playerA) * player_linear_speed;
-		double delta_y = sin(playerA) * player_linear_speed;
-		playerMovementControl(-delta_x, -delta_y);
-	}
+	if (currentKeyStates[SDL_SCANCODE_UP])
+	{
+		double d_x = cos(plA) * sp, d_y = sin(plA) * sp;
 
-	if(currentKeyStates[SDL_SCANCODE_LEFT]){
-		double delta_x = cos(playerA - 1.57) * player_linear_speed;
-		double delta_y = sin(playerA - 1.57) * player_linear_speed;
-		playerMovementControl(delta_x, delta_y);
+		playerMovementControl(d_x, d_y);
 	}
+	if (currentKeyStates[SDL_SCANCODE_DOWN])
+	{
+		double d_x = cos(plA) * sp, d_y = sin(plA) * sp;
 
-	if(currentKeyStates[SDL_SCANCODE_RIGHT]){
-		double delta_x = cos(playerA + 1.57) * player_linear_speed;
-		double delta_y = sin(playerA + 1.57) * player_linear_speed;
-		playerMovementControl(delta_x, delta_y);
+		playerMovementControl(-d_x, -d_y);
 	}
-	return false;
+	if (currentKeyStates[SDL_SCANCODE_LEFT])
+	{
+		double d_x = cos(plA - 1.57) * sp, d_y = sin(plA - 1.57) * sp;
+
+		playerMovementControl(d_x, d_y);
+	}
+	if (currentKeyStates[SDL_SCANCODE_RIGHT])
+	{
+		double d_x = cos(plA + 1.57) * sp, d_y = sin(plA + 1.57) * sp;
+
+		playerMovementControl(d_x, d_y);
+	}
+	return (false);
 }
 
-/*
+/**
  * drawFloor - a function to draw a floor
- * Description:
+ * Description: draw a floor coloer
  * Return: void
  *
  */
- 
-void drawFloor(){
-	for (y = 0; y < SCREEN_HEIGHT / 2; y++){
+
+void drawFloor(void)
+{
+	const int SCR_WIDTH = 640, SCREEN_HEIGHT = 480;
+
+	for (y = 0; y < SCREEN_HEIGHT / 2; y++)
+	{
 		Uint8 color = 255.0 - 255.0 * ((float)y / (float)SCREEN_HEIGHT);
 		Uint32 floorColor = ((0 + color) << 16) + 255;
-		for (x = 0; x < SCREEN_WIDTH; x++){
-			*(((Uint32*) pixels) + SCREEN_WIDTH * (SCREEN_HEIGHT - 1) - y * SCREEN_WIDTH + x) = floorColor;
+
+		for (x = 0; x < SCR_WIDTH; x++)
+		{
+			*(((Uint32 *) pixels) + SCR_WIDTH * (SCREEN_HEIGHT - 1)
+					- y * SCR_WIDTH + x) = floorColor;
 		}
 	}
 }
 
-/*
+/**
  * drawCiel - a function to draw a ciel
- * Description:
+ * Description: draw a ciel from color
  * Return: void
  *
  */
-void drawCiel(){
-	for (y = 0; y < SCREEN_HEIGHT / 2; y++){
+void drawCiel(void)
+{
+	const int SCREEN_WIDTH = 640, SCREEN_HEIGHT = 480;
+
+	for (y = 0; y < SCREEN_HEIGHT / 2; y++)
+	{
 		Uint8 color = 255.0 - 255.0 * ((float)y / (float)SCREEN_HEIGHT);
 		Uint32 ceilColor = ((0 + color) << 8) + 255;
-		for (x = 0; x < SCREEN_WIDTH; x++){
-			*(((Uint32*) pixels) + SCREEN_WIDTH * y + x) = ceilColor;
-		}
-	}
 
+		for (x = 0; x < SCREEN_WIDTH; x++)
+			*(((Uint32 *) pixels) + SCREEN_WIDTH * y + x) = ceilColor;
+	}
 }
 
-/* 
+/**
  * drawWall - a function that draws a wall
- * Description:
+ * Description: draws a wall
  * Return: Void
  *
  */
-  
-void drawWall(){
-	const double DIST_TO_PLANE_OF_PROJECTION= 1.0;
-	for (x = 0; x < SCREEN_WIDTH; x++){
-		double playerEyerayAngle, rayCastAngle, horizontal_distance, wallcoord;
+
+void drawWall(void)
+{
+	const int S_W = 640, S_H = 480;
+	const double DIST_TO_P_PRO = 1.0;
+
+	for (x = 0; x < S_W; x++)
+	{
+		double plyAng, rayCastAngle, hor_dis, wallcoord;
 		Point rayReturn;
 		int wallHeight, actualWAllHeight, wallY;
 		Uint8 color;
-		playerEyerayAngle = atan((x - SCREEN_WIDTH / 2.0) / (SCREEN_WIDTH / 2.0));
-		rayCastAngle = playerA + playerEyerayAngle;
+
+		plyAng = atan((x - S_W / 2.0) / (S_W / 2.0));
+		rayCastAngle = plA + plyAng;
 		rayReturn = distanceToWall(playerPos, rayCastAngle);
-		horizontal_distance = rayReturn.x * cos(playerEyerayAngle);
-		wallHeight = DIST_TO_PLANE_OF_PROJECTION / horizontal_distance * SCREEN_HEIGHT * 1.5;
+		hor_dis = rayReturn.x * cos(plyAng);
+		wallHeight = DIST_TO_P_PRO / hor_dis * S_H * 1.5;
 		actualWAllHeight = wallHeight;
-		wallHeight = (wallHeight > SCREEN_HEIGHT) ? SCREEN_HEIGHT : wallHeight;
-		wallY = (int)((SCREEN_HEIGHT - wallHeight) / 2);
+		wallHeight = (wallHeight > S_H) ? S_H : wallHeight;
+		wallY = (int)((S_H - wallHeight) / 2);
 		wallcoord = rayReturn.y;
-		for (y = wallY; y < wallHeight + wallY; y++){
-			int texture_x = 0, texture_y = 0;
+		for (y = wallY; y < wallHeight + wallY; y++)
+		{
+			int tex_x = 0, tex_y = 0;
 			Uint8 r, g, b;
 			Uint32 wallColor = 0;
-			if (horizontal_distance > DIST_TO_PLANE_OF_PROJECTION){
-				texture_x = wallcoord * 127;
-				texture_y = (y - wallY) / (float)wallHeight * 127;
-			}else{
-				texture_x = wallcoord * 127;
-				texture_y = ((actualWAllHeight - wallHeight) / 2.0 + y) / (float)actualWAllHeight * 127;
+
+			if (hor_dis > DIST_TO_P_PRO)
+			{
+				tex_x = wallcoord * 127;
+				tex_y = (y - wallY) / (float)wallHeight * 127;
 			}
-			r = *(((Uint8*) (wall_img_surf->pixels)) + texture_y * 3*128 + 3*texture_x + 0);
-			g = *(((Uint8*) (wall_img_surf->pixels)) + texture_y * 3*128 + 3*texture_x + 1);
-			b = *(((Uint8*) (wall_img_surf->pixels)) + texture_y * 3*128 + 3*texture_x + 2);
-			color= 255.0 * exp(-horizontal_distance/30);
+			else
+			{
+				tex_x = wallcoord * 127;
+				tex_y = ((actualWAllHeight - wallHeight) / 2.0 + y)
+					/ (float)actualWAllHeight * 127;
+			}
+			r = *(((Uint8 *) (w_surf->pixels)) + tex_y * 3 * 128 + 3 * tex_x + 0);
+			g = *(((Uint8 *) (w_surf->pixels)) + tex_y * 3 * 128 + 3 * tex_x + 1);
+			b = *(((Uint8 *) (w_surf->pixels)) + tex_y * 3 * 128 + 3 * tex_x + 2);
+			color = 255.0 * exp(-hor_dis / 30);
 			r *= color / 255.0, g *= color / 255.0, b *= color / 255.0;
 			wallColor = (((((b << 8) + g) << 8) + r) << 8) + 255;
-			*(((Uint32*) pixels) + SCREEN_WIDTH * y + x) = wallColor;
+			*(((Uint32 *) pixels) + S_W * y + x) = wallColor;
 		}
-
 	}
 }
 
@@ -162,9 +162,11 @@ void drawWall(){
  * Return: void
  *
  */
- 
-void drawScreen(SDL_Renderer *renderer, SDL_Texture *screen){
+
+void drawScreen(SDL_Renderer *renderer, SDL_Texture *screen)
+{
 	int pitch;
+
 	SDL_LockTexture(screen, NULL, &pixels, &pitch);
 	drawFloor();
 	drawCiel();
@@ -172,5 +174,4 @@ void drawScreen(SDL_Renderer *renderer, SDL_Texture *screen){
 	whereIsThePLayer(renderer);
 	SDL_UnlockTexture(screen);
 	SDL_RenderCopy(renderer, screen, NULL, NULL);
-
 }
